0dacfc08c755f9d8d0bb4175d06d49b142288512
Ryan Racioppo <ryanracioppo@gmail.com>
2017-07-30 19:43:25 -0400

    fix navbar

--- app/models/heat.rb
+++ app/models/heat.rb
@@ -5,27 +5,27 @@ class Heat < ActiveRecord::Base
   	validates :competition_id, presence: true
 
   	def heatFinished(competition, players)
-		winnersTable = players.group_by { |r| r[:score] }
-	      .max_by(self.winner_count, &:first)
-	      .flat_map(&:last)
-	      .sort_by { |r| -r[:score] }
-	  currentHeatCount = Heat.where(round_number: self.round_number).count
-	  nextHeatCount = currentHeatCount/self.winner_count
+	winnersTable = players.group_by { |r| r[:score] }
+		.max_by(self.winner_count, &:first)
+		.flat_map(&:last)
+		.sort_by { |r| -r[:score] }
+	currentHeatCount = Heat.where(round_number: self.round_number).count
+	nextHeatCount = currentHeatCount/self.winner_count
 
-	  newIndex = ((self.index.to_f/self.player_count).ceil.to_i)
-	  p "ADDING NEW PLAYERS TO NEXT HEAT"
-	  p newIndex
-      moveToHeat = Heat.where(round_number: self.round_number+1, index: newIndex, competition_id: competition.id).first
-      p winnersTable
-      if moveToHeat != nil
-	      winnersTable.each do |x|
-	      	Player.create!(heat_id: moveToHeat.id, gamertag: x.gamertag, competition_id: competition.id)
-	      end
-	  else
-	      winnersTable.each do |x|
-	      	Player.create!(heat_id: -1, gamertag: x.gamertag, competition_id: competition.id)
-	      end
+	newIndex = ((self.index.to_f/self.player_count).ceil.to_i)
+	p "ADDING NEW PLAYERS TO NEXT HEAT"
+	p newIndex
+	moveToHeat = Heat.where(round_number: self.round_number+1, index: newIndex, competition_id: competition.id).first
+	p winnersTable
+	if moveToHeat != nil
+	  winnersTable.each do |x|
+	  	Player.create!(heat_id: moveToHeat.id, gamertag: x.gamertag, competition_id: competition.id)
 	  end
+	else
+	  winnersTable.each do |x|
+	  	Player.create!(heat_id: -1, gamertag: x.gamertag, competition_id: competition.id)
+	  end
+	end
 
 
   	end
